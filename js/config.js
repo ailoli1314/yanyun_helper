7d1bfe5b5abaf08258413294105fd93c888c1caa
v_zhangguibin <v_zhangguibin@baidu.com>
2026-03-18 18:36:12 +0800

    新增资源管理

---
+++ js/config.js
@@ -0,0 +1,28 @@
+/**
+ * 统一资源配置
+ * 所有资源URL在此集中管理，便于后续替换
+ */
+const RESOURCE_CONFIG = {
+    // 视频资源
+    video: {
+        demo: "//player.bilibili.com/player.html?bvid=BV号待替换"
+    },
+
+    // 下载资源
+    download: {
+        app: "蓝奏云链接待替换"
+    },
+
+    // 图片资源
+    images: {
+        ocr: "SM.MS图片链接待替换",
+        record: "SM.MS图片链接待替换",
+        trigger: "SM.MS图片链接待替换",
+        auth: "SM.MS图片链接待替换"
+    }
+};
+
+// 导出配置
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { RESOURCE_CONFIG };
+}
